0c09938c16dcbecd079b21089145be8e183a7c19
root <root@vm1>
2024-06-08 00:19:26 +0200

    Comprobar si el usuario ha insertado el fichero sesion adecuado para analizar la sesion

--- src/paginas/AnalyzePage/index.jsx
+++ src/paginas/AnalyzePage/index.jsx
@@ -144,7 +144,7 @@ const AnalyzePage = () => {
                 try {
                     sesionObj = JSON.parse(e.target.result);
                 } catch (error) {
-                    alert('Error parsing JSON file');
+                    alert('The file must be in the content manager session format.');
                 }
 
                 //  Comprobamos que en el objeto hay 
